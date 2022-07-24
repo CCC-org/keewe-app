c4d24b4824ee6b68f1493eeb3304b259783ac10d
ysh4296 <dbtmdgns4296@naver.com>
2022-07-25 00:40:23 +0900

    테마적용

--- components/BlackNextButton.tsx
+++ components/BlackNextButton.tsx
@@ -1,52 +1,40 @@
-import {
-	View,
-	Text,
-	StyleSheet,
-	KeyboardAvoidingView,
-	Platform,
-} from 'react-native';
-import React, { useEffect } from 'react';
-import { IconButton } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { IconButton, useTheme } from 'react-native-paper';
 
 interface BlackNextBottonProps {
-	isActive: boolean;
-	handlePress: () => void;
+  isActive: boolean;
+  handlePress: () => void;
 }
 
 const BlackNextButton = (props: BlackNextBottonProps) => {
-	const { isActive, handlePress } = props;
-
-	
-
-	return (
-		<View
-			style={{
-				...styles.nextButtonContainer,
-				backgroundColor: isActive ? 'black' : 'grey',
-			}}
-		>
-			<IconButton
-				icon="arrow-right"
-				color="white"
-				size={24}
-				onPress={handlePress}
-			/>
-		</View>
-	);
+  const { isActive, handlePress } = props;
+  const theme = useTheme();
+  return (
+    <View
+      style={{
+        ...styles.nextButtonContainer,
+        backgroundColor: theme.colors.graphic.black,
+        opacity: isActive ? 1 : 0.2,
+      }}
+    >
+      <IconButton icon="arrow-right" color="white" size={24} onPress={handlePress} />
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-	nextButtonContainer: {
-		position: 'absolute',
-		bottom: 20,
-		right: 20,
-		width: 64,
-		height: 64,
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 12,
-	},
+  nextButtonContainer: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 64,
+    height: 64,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 12,
+  },
 });
 
 export default BlackNextButton;
--- components/TextInputDetail.tsx
+++ components/TextInputDetail.tsx
@@ -1,50 +1,34 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import React from 'react';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { TextInput } from 'react-native-paper';
+import { TextInput, Text, useTheme } from 'react-native-paper';
 
 interface TextInputDetailProps {
-	setInputValue: (input: string) => void;
-	infoText: string;
-	inputValue: string;
-	label: string;
-	placeholder: string;
+  setInputValue: (input: string) => void;
+  infoText: string;
+  inputValue: string;
+  label: string;
+  placeholder: string;
 }
 
-let name:string = 'jihoon'
-let count:number = 1;
-let isTrue:boolean = true;
-const arr:number[] = [1,2,3,4]
-
-
 const TextInputDetail = (props: TextInputDetailProps) => {
-	const { setInputValue, inputValue, infoText, label, placeholder } = props;
-	return (
-		<>
-			<View>
-				<Text style={styles.detailInfoText}>{infoText}</Text>
-			</View>
-			<TextInput
-				label={label}
-				value={inputValue}
-				placeholder={placeholder}
-				onChangeText={(inputValue) => setInputValue(inputValue)}
-				underlineColor="#486006"
-				activeUnderlineColor="#486006"
-				style={{ margin: 10, backgroundColor: 'white' }}
-			/>
-		</>
-	);
+  const { setInputValue, inputValue, infoText, label, placeholder } = props;
+  const theme = useTheme();
+  return (
+    <>
+      <View>
+        <Text style={theme.fonts.text.display}>{infoText}</Text>
+      </View>
+      <TextInput
+        label={label}
+        value={inputValue}
+        placeholder={placeholder}
+        onChangeText={(inputValue) => setInputValue(inputValue)}
+        underlineColor="#486006"
+        activeUnderlineColor="#486006"
+        style={{ margin: 10, backgroundColor: 'white' }}
+      />
+    </>
+  );
 };
-
-const styles = StyleSheet.create({
-	detailInfoText: {
-		marginLeft: 10,
-		marginTop: 0,
-		fontSize: 32,
-		fontWeight: 'bold',
-		textAlign: 'left',
-	},
-});
 
 export default TextInputDetail;
