b54559d8a7c398fc51cd83a14007117eabf03a0c
akdlsz21 <akdlsz21@gmail.com>
2022-07-24 18:26:50 +0900

    navigation setup complete

--- App.tsx
+++ App.tsx
@@ -1,21 +1,30 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import NicknameCreationScreen from './screens/NicknameCreationScreen';
+import Navigation, { RootScreen } from './navigation';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Text } from 'react-native-paper';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-	const isLoadingComplete = useCachedResources();
-	const colorScheme = useColorScheme();
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
 
-	if (!isLoadingComplete) {
-		return null;
-	} else {
-		return (
-			<NavigationContainer>
-				<NicknameCreationScreen />
-				<StatusBar />
-			</NavigationContainer>
-		);
-	}
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Root" component={RootScreen} />
+          <Stack.Screen name="NicknameCreation" component={NicknameCreationScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
--- screens/NicknameCreationScreen.tsx
+++ screens/NicknameCreationScreen.tsx
@@ -4,54 +4,51 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { TextInput } from 'react-native-paper';
 import TextInputDetail from '../components/TextInputDetail';
 import BlackNextButton from '../components/BlackNextButton';
-const detailInfoTextProp = `반가워요 키위새님${`\n`}닉네임이 무엇인가요?`;
+const detailInfoTextProp = `반가워요 키위새님${'\n'}닉네임이 무엇인가요?`;
 
 const NicknameCreationScreen = () => {
-	const [nickname, setNickname] = useState<string>('');
-	const [isLengthGreaterThanFour, setIsLengthGreaterThanFour] =
-		useState(false);
+  const [nickname, setNickname] = useState<string>('');
+  const [isLengthGreaterThanFour, setIsLengthGreaterThanFour] = useState(false);
 
-	useEffect(() => {
-		if (nickname.length > 4) setIsLengthGreaterThanFour(true);
-		else setIsLengthGreaterThanFour(false);
-	}, [nickname]);
+  useEffect(() => {
+    if (nickname.length > 4) setIsLengthGreaterThanFour(true);
+    else setIsLengthGreaterThanFour(false);
+  }, [nickname]);
 
-	function handleNextButtonPress(){
-		alert('다음으로 가기 검은색버튼 눌림. 닉네임: ' + nickname);
-	}
+  function handleNextButtonPress() {
+    alert('다음으로 가기 검은색버튼 눌림. 닉네임: ' + nickname);
+  }
 
-	return (
-		<SafeAreaView style={styles.container}>
-			<View style={{ backgroundColor: 'orange' }}>
-				<View style={styles.backArrowContainer}>
-					<Text>Place where back arrow should be inserted</Text>
-				</View>
-				<TextInputDetail
-					setInputValue={setNickname}
-					infoText={detailInfoTextProp}
-					inputValue={nickname}
-					label="닉네임"
-					placeholder="닉네임을 입력하세요"
-				/>
-			</View>
-			<BlackNextButton isActive={isLengthGreaterThanFour} 
-				handlePress={handleNextButtonPress}
-			/>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={{ backgroundColor: 'orange' }}>
+        <View style={styles.backArrowContainer}>
+          <Text>Place where back arrow should be inserted</Text>
+        </View>
+        <TextInputDetail
+          setInputValue={setNickname}
+          infoText={detailInfoTextProp}
+          inputValue={nickname}
+          label="닉네임"
+          placeholder="닉네임을 입력하세요"
+        />
+      </View>
+      <BlackNextButton isActive={isLengthGreaterThanFour} handlePress={handleNextButtonPress} />
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: 'green',
-	},
-	backArrowContainer: {
-		fontSize: 32,
-		height: 50,
-		fontWeight: 'bold',
-		backgroundColor: 'red',
-	},
+  container: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+  backArrowContainer: {
+    fontSize: 32,
+    height: 50,
+    fontWeight: 'bold',
+    backgroundColor: 'red',
+  },
 });
 
 export default NicknameCreationScreen;
