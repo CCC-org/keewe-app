331592621b1781f5e65c98ef1e8acb082bf3d40c
akdlsz21 <akdlsz21@gmail.com>
2022-07-29 23:57:58 +0900

    made styleSheet useTheme available with function

--- src/screens/onboarding/ActivitySelectionScreen.tsx
+++ src/screens/onboarding/ActivitySelectionScreen.tsx
@@ -1,12 +1,42 @@
-import { Text, Card } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
+import { Text, useTheme } from 'react-native-paper';
 import React from 'react';
 
 const ActivitySelectionScreen = () => {
+  const themeProp = useTheme();
+  const styles = makeStyles(themeProp);
+
   return (
-    <Card>
-      <Text>ActivitySelectionScreen</Text>
-    </Card>
+    <View style={styles.container}>
+      <Text style={styles.screenTitle}>활동분야를 알려주시오</Text>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  screenTitle: {
+    fontSize: 30,
+  },
+});
+
+function makeStyles(theme: ReactNativePaper.Theme) {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: theme.colors.brand.surface.container,
+    },
+    screenTitle: {
+      fontSize: 30,
+    },
+  });
+}
 
 export default ActivitySelectionScreen;
