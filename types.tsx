486f18c3581e3f0f3676b1386f3aaa9134ec3b44
akdlsz21 <akdlsz21@gmail.com>
2022-08-25 21:46:48 +0900

    delete unused files

--- types.tsx
+++ types.tsx
@@ -23,7 +23,6 @@ export type RootStackParamList = {
   SignUp: undefined;
   GetToken: undefined;
   Login: undefined;
-  Temp: undefined;
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
