0d4efdf40a0a7c9be9a1c3b3d4227b18fd003876
Cadi <cadi.you@kakaoenterprise.com>
2022-08-26 17:06:10 +0900

    tag 디자인 적용

--- src/components/buttons/tag.tsx
+++ src/components/buttons/tag.tsx
@@ -1,32 +1,43 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { useTheme } from 'react-native-paper';
 
-interface tagProps {
+interface TagProps {
   title: string;
   isSelected: boolean;
   onClick: () => void;
 }
 
-const tag = ({ title, isSelected, onClick }: tagProps) => {
+const Tag = ({ title, isSelected, onClick }: TagProps) => {
   const theme = useTheme();
   return (
     <View
       style={[
         styles.tag,
-        { backgroundColor: theme.colors.graphic.green, opacity: isSelected ? 1 : 0 },
+        {
+          backgroundColor: isSelected === true ? theme.colors.graphic.green : 'transparent',
+          borderColor: `${theme.colors.graphic.black}10`,
+        },
       ]}
     >
-      {title}
+      <Text style={theme.fonts.text.body2.bold}>{title}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   tag: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignSelf: 'center',
+    paddingTop: 6,
+    paddingBottom: 6,
+    paddingRight: 12,
+    paddingLeft: 12,
     borderRadius: 12,
-    border: 'solid 1px black',
+    borderWidth: 1,
+    borderStyle: 'solid',
   },
 });
 
-export default tag;
+export default Tag;
