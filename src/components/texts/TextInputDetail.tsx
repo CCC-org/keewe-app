34514f55fe3f4ef85e5482615cb699298b412f11
watchiswatch <chchih99@gmail.com>
2022-09-05 20:15:19 +0900

    TextInput refactoring

--- src/components/texts/TextInputDetail.tsx
+++ src/components/texts/TextInputDetail.tsx
@@ -23,7 +23,7 @@ const TextInputDetail = (props: TextInputDetailProps) => {
   const handleTextInputButton = () => setInputValue('');
 
   useEffect(() => {
-    if (inputValue.length > 1) {
+    if (inputValue.length > 0) {
       setIsButtonEnabled(true);
     } else {
       setIsButtonEnabled(false);
@@ -40,20 +40,24 @@ const TextInputDetail = (props: TextInputDetailProps) => {
       setInputValueColor('red');
     } else if (focused) {
       setInputUnderlineColor('#486006');
-      setInputValueColor('black');
+      setInputValueColor('#121314');
     } else if (!focused) {
-      setInputUnderlineColor('grey');
-      setInputValueColor('grey');
+      setInputUnderlineColor('rgba(12,13,14,0.1)');
+      setInputValueColor('rgba(12,13,14,0.1)');
     }
   }, [inputValue, focused]);
 
   return (
     <>
       <View>
-        <Text style={theme.fonts.text.caption1}>{infoText}</Text>
+        <Text style={{ ...theme.fonts.text.caption1, marginLeft: 12, opacity: 0.5 }}>
+          {infoText}
+        </Text>
       </View>
       <TextInput
         label={label}
+        autoFocus={true}
+        selectionColor={'black'}
         right={
           <TextInput.Icon
             style={{ opacity: isButtonEnabled ? 0.2 : 0 }}
@@ -70,9 +74,11 @@ const TextInputDetail = (props: TextInputDetailProps) => {
           fontWeight: '600',
           fontSize: 22,
           backgroundColor: 'white',
+          marginHorizontal: 12,
         }}
         onFocus={() => setFocused(true)}
         onEndEditing={() => setFocused(false)}
+        placeholderTextColor={'rgba(18, 19, 20, 0.3)'}
         theme={{
           colors: {
             text: inputValueColor,
