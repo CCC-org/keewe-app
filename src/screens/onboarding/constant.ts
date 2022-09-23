287dafbe0de35bd8f5b84f464fdb1b7a1c6d2230
ysh4296 <dbtmdgns4296@naver.com>
2022-09-23 22:31:43 +0900

    animation 조정

--- src/screens/onboarding/constant.ts
+++ src/screens/onboarding/constant.ts
@@ -8,9 +8,9 @@ import theme from '../../theme/light';
 
 export const REACTIONS: Record<string, any>[] = [
   { xml: ClapRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.0], color: theme.colors.graphic.sky },
-  { xml: HeartRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.15], color: theme.colors.graphic.green },
-  { xml: SadRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.3], color: theme.colors.graphic.purple },
-  { xml: SurpriseRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.45], color: theme.colors.graphic.coral },
-  { xml: EyesRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.6], color: theme.colors.graphic.marine },
-  { xml: FireRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.75], color: theme.colors.graphic.orange },
+  { xml: HeartRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.12], color: theme.colors.graphic.green },
+  { xml: SadRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.24], color: theme.colors.graphic.purple },
+  { xml: SurpriseRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.36], color: theme.colors.graphic.coral },
+  { xml: EyesRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.48], color: theme.colors.graphic.marine },
+  { xml: FireRoundOffXml, bezier: [0.5, 1.8, 0.4, 1.6], color: theme.colors.graphic.orange },
 ];
