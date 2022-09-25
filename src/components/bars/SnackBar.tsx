29d9b7d040372aed5d398a5080de6a7d0216c746
watchiswatch <chchih99@gmail.com>
2022-09-25 21:25:04 +0900

    theme 적용

--- src/components/bars/SnackBar.tsx
+++ src/components/bars/SnackBar.tsx
@@ -1,6 +1,7 @@
 import { Pressable, Text } from 'react-native';
 import React from 'react';
 import { Snackbar } from 'react-native-paper';
+import { useTheme } from 'react-native-paper';
 
 interface SnackBarProps {
   visible: boolean;
@@ -19,6 +20,7 @@ const SnackBar = ({
   onPressSnackBar,
   onPressAction,
 }: SnackBarProps) => {
+  const theme = useTheme();
   return (
     <>
       <Pressable onPress={onPressSnackBar}>
@@ -28,7 +30,7 @@ const SnackBar = ({
           onDismiss={onDismiss}
           action={{
             label: actionText,
-            color: '#FF7163',
+            color: theme.colors.graphic.coral,
           }}
           style={{ width: 343, height: 60, borderRadius: 8, paddingHorizontal: 10 }}
         >
