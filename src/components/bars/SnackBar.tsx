0fe59d54ad81aa1973e90cb8fac1c86b0bc06ebf
akdlsz21 <akdlsz21@gmail.com>
2022-10-16 15:24:01 +0900

    fixed snackbar margin difference

--- src/components/bars/SnackBar.tsx
+++ src/components/bars/SnackBar.tsx
@@ -2,39 +2,44 @@ import { Pressable, Text } from 'react-native';
 import React from 'react';
 import { Snackbar } from 'react-native-paper';
 import { useTheme } from 'react-native-paper';
+import { FontText } from '../texts/StyledText';
 
 interface SnackBarProps {
   visible: boolean;
   actionText: string;
   text: string;
-  onDismiss: () => void;
+  onDismiss?: () => void;
   onPressSnackBar?: () => void;
   onPressAction?: () => void;
+  style?: any;
 }
 
-const SnackBar = ({
-  visible,
-  text,
-  actionText,
-  onDismiss,
-  onPressSnackBar,
-  onPressAction,
-}: SnackBarProps) => {
+const SnackBar = (props: SnackBarProps) => {
   const theme = useTheme();
+  const { visible, text, actionText, onDismiss, onPressSnackBar, onPressAction } = props;
+  console.log('snackbar valid');
   return (
     <>
-      <Pressable onPress={onPressSnackBar}>
+      <Pressable onPress={onPressSnackBar} style={[props.style, {}]}>
         <Snackbar
           duration={3000}
           visible={visible}
-          onDismiss={onDismiss}
+          // eslint-disable-next-line @typescript-eslint/no-empty-function
+          onDismiss={onDismiss || (() => {})}
           action={{
             label: actionText,
             color: theme.colors.graphic.coral,
           }}
-          style={{ width: 343, height: 60, borderRadius: 8, paddingHorizontal: 10 }}
+          style={{
+            width: 343,
+            height: 60,
+            borderRadius: 8,
+            paddingHorizontal: 10,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+          }}
         >
-          <Text>{text}</Text>
+          <FontText>{text}</FontText>
         </Snackbar>
       </Pressable>
     </>
