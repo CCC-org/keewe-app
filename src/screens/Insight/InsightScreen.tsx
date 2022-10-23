590da56b75d9860c09cf0afb6a4550cc2f563b4b
ysh4296 <dbtmdgns4296@naver.com>
2022-10-23 22:34:38 +0900

    merge

--- src/screens/Insight/InsightScreen.tsx
+++ src/screens/Insight/InsightScreen.tsx
@@ -1,36 +1,72 @@
-import React from 'react';
+import { Pressable, StyleSheet, View } from 'react-native';
+import React, { useLayoutEffect } from 'react';
 import Profile from '../../components/profile/Profile';
+import { Feather } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
 
-const InsightScreen = ({ navigation, route }) => {
+const InsightScreen = ({ navigation }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <View style={styles.headerRight}>
+            <Pressable onPress={() => alert('bookmark')}>
+              <Feather name="bookmark" size={24} color="black" />
+            </Pressable>
+            <Pressable onPress={() => alert('share')}>
+              <Feather name="share" size={24} color="black" />
+            </Pressable>
+            <Pressable onPress={() => alert('three dots')}>
+              <Entypo name="dots-three-vertical" size={24} color="black" />
+            </Pressable>
+          </View>
+        );
+      },
+    });
+  });
   return (
-    <Profile
-      nickname="nickname"
-      title="title"
-      self={false}
-      follow={true}
-      interests={[
-        {
-          name: 'interest',
-        },
-        {
-          name: 'interest',
-        },
-        {
-          name: 'interest',
-        },
-        {
-          name: 'interest',
-        },
-        {
-          name: 'interest',
-        },
-        {
-          name: 'interest',
-        },
-      ]}
-      createdAt="createdAt"
-    />
+    <>
+      <Profile
+        nickname="nickname"
+        title="title"
+        self={false}
+        follow={true}
+        interests={[
+          {
+            name: 'interest',
+          },
+          {
+            name: 'interest',
+          },
+          {
+            name: 'interest',
+          },
+          {
+            name: 'interest',
+          },
+          {
+            name: 'interest',
+          },
+          {
+            name: 'interest',
+          },
+        ]}
+        createdAt="createdAt"
+      />
+      {/* Insight text, link card, emoticons, etc.. */}
+      {/* reply etc.. */}
+    </>
   );
 };
 
 export default InsightScreen;
+
+const styles = StyleSheet.create({
+  headerRight: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: 100,
+    marginRight: 20,
+  },
+});
