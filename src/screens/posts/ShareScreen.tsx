01f31ea3c22bcf647a0d25feb0ad7290d45bf99c
akdlsz21 <akdlsz21@gmail.com>
2022-10-23 11:43:34 +0900

    added fonts and styles changed

--- src/screens/posts/ShareScreen.tsx
+++ src/screens/posts/ShareScreen.tsx
@@ -2,19 +2,39 @@ import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
 import React, { useRef, useState } from 'react';
 import { FontText } from '../../components/texts/StyledText';
 import { captureRef } from 'react-native-view-shot';
-// import Share from 'react-native-share';
 import * as Sharing from 'expo-sharing';
+import ColorSelectRadioButton from '../../components/buttons/ColorSelectRadioButton';
+import ShareButton from '../../components/buttons/ShareButton';
+import { Feather } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+const falseObject = {
+  first: false,
+  second: false,
+  third: false,
+};
 
 const ShareScreen = () => {
   const [color, setColor] = useState('#f1f1e9');
+  const [buttonColorSelected, setButtonColorSelected] = useState({
+    first: true,
+    second: false,
+    third: false,
+  });
   const viewRef = useRef(null);
 
   const shareImage = async () => {
     const uri = await captureRef(viewRef, {
       format: 'png',
-      quality: 0.8,
+      quality: 1,
     });
-    await Sharing.shareAsync(uri, { mimeType: 'image/png' });
+    await Sharing.shareAsync(uri, { mimeType: 'image/jpg' });
+  };
+
+  const handleSelectColor = (position: string, color: string) => {
+    setColor(color);
+    setButtonColorSelected((prev) => {
+      return { ...falseObject, [position]: true };
+    });
   };
 
   return (
@@ -25,25 +45,43 @@ const ShareScreen = () => {
           <FontText
             style={{ ...styles.text, ...{ color: color === '#f1f1e9' ? 'black' : 'white' } }}
           >
-            ShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreen
+            ShareScreenShareScreenShareScreenSha231312reScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenSha555reScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareSc123r123123ee5hareScreenShareScreenShareScreenShareScreenShareScreenShareScre123enS123hareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreen32ShareScreenShareScreenS321321hareScreenShare3213ScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareSc21312312reenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreenShareScreen
           </FontText>
         </ScrollView>
       </View>
-      <View style={styles.buttonContainer}>
-        <Pressable onPress={() => setColor('#f1f1e9')}>
-          <FontText>01</FontText>
-        </Pressable>
-        <Pressable onPress={() => setColor('#486006')}>
-          <FontText>02</FontText>
-        </Pressable>
-        <Pressable onPress={() => setColor('rgba(18,19,20,0.8)')}>
-          <FontText>03</FontText>
-        </Pressable>
-      </View>
+      <View style={styles.bottom}>
+        <View style={styles.buttonContainer}>
+          <Pressable onPress={() => handleSelectColor('first', '#f1f1e9')}>
+            <ColorSelectRadioButton color="#f1f1e9" selected={buttonColorSelected.first} />
+          </Pressable>
+          <Pressable onPress={() => handleSelectColor('second', '#486006')}>
+            <ColorSelectRadioButton color="#486006" selected={buttonColorSelected.second} />
+          </Pressable>
+          <Pressable onPress={() => handleSelectColor('third', 'rgba(18,19,20,0.8)')}>
+            <ColorSelectRadioButton
+              color="rgba(18,19,20,0.8)"
+              selected={buttonColorSelected.third}
+            />
+          </Pressable>
+        </View>
 
-      <Pressable onPress={shareImage}>
-        <Text>Share</Text>
-      </Pressable>
+        <View style={styles.shareButtonContainer}>
+          <Pressable onPress={shareImage}>
+            <ShareButton
+              text={'공유하기'}
+              icon={<Feather name="share" size={24} color="black" />}
+            />
+          </Pressable>
+          <Pressable>
+            <ShareButton
+              text={'이미지 저장'}
+              icon={
+                <MaterialCommunityIcons name="download-circle-outline" size={24} color="black" />
+              }
+            />
+          </Pressable>
+        </View>
+      </View>
     </>
   );
 };
@@ -52,11 +90,12 @@ export default ShareScreen;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0.4,
+    flex: 0.6,
     backgroundColor: '#f1f1e9',
     alignItems: 'center',
     justifyContent: 'center',
-    padding: 40,
+    paddingHorizontal: 32,
+    paddingVertical: 20,
   },
   profileContainer: {
     borderWidth: 1,
@@ -72,12 +111,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     width: '50%',
     padding: 20,
-    borderWidth: 1,
     marginLeft: 'auto',
     marginRight: 'auto',
   },
   text: {
     fontWeight: '600',
     lineHeight: 32,
+  },
+  bottom: {
+    flex: 0.6,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  shareButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '55%',
+    marginTop: 20,
   },
 });
