0101b35aedbd07330dc42240330f159836729346
ysh4296 <dbtmdgns4296@naver.com>
2022-11-13 21:36:38 +0900

    미사용 import 삭제

--- src/components/buttons/BottomFixButton.tsx
+++ src/components/buttons/BottomFixButton.tsx
@@ -1,14 +1,5 @@
-import {
-  StyleSheet,
-  Text,
-  View,
-  Pressable,
-  KeyboardAvoidingView,
-  Platform,
-  Dimensions,
-  LayoutChangeEvent,
-} from 'react-native';
-import React, { useState } from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import React from 'react';
 import { useTheme } from 'react-native-paper';
 
 interface BottomFixButtonProps {
