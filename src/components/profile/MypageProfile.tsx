3aeb82be4ef1fb9e08ea5e2973e133496e318efe
watchiswatch <chchih99@gmail.com>
2022-11-18 21:09:56 +0900

    top section 틀 짜기

--- src/components/profile/MypageProfile.tsx
+++ src/components/profile/MypageProfile.tsx
@@ -9,15 +9,37 @@ const MypageProfile = () => {
     <View style={styles.container}>
       <ProfileAvatar size={84} />
       <View style={{ marginLeft: 12 }}>
-        <Text style={{ ...theme.fonts.text.headline1, color: '#121314cc' }}>닉네임</Text>
-        <Text style={{ ...theme.fonts.text.body2.bold, color: '#12131480', marginTop: 4 }}>
+        <Text style={{ ...theme.fonts.text.headline1, color: `${theme.colors.graphic.black}cc` }}>
+          닉네임
+        </Text>
+        <Text
+          style={{
+            ...theme.fonts.text.body2.bold,
+            color: `${theme.colors.graphic.black}80`,
+            marginTop: 4,
+          }}
+        >
           타이틀
         </Text>
         <View style={{ flexDirection: 'row', marginTop: 8 }}>
-          <Text style={{ ...theme.fonts.text.body1.regular, color: '#121314cc' }}>팔로워</Text>
-          <Text style={{ ...theme.fonts.text.body1.bold, color: '#121314' }}> 123 </Text>
-          <Text style={{ ...theme.fonts.text.body1.regular, color: '#121314cc' }}> 팔로잉 </Text>
-          <Text style={{ ...theme.fonts.text.body1.bold, color: '#121314' }}>50K</Text>
+          <Text
+            style={{ ...theme.fonts.text.body1.regular, color: `${theme.colors.graphic.black}cc` }}
+          >
+            팔로워
+          </Text>
+          <Text style={{ ...theme.fonts.text.body1.bold, color: theme.colors.graphic.black }}>
+            {' '}
+            123{' '}
+          </Text>
+          <Text
+            style={{ ...theme.fonts.text.body1.regular, color: `${theme.colors.graphic.black}cc` }}
+          >
+            {' '}
+            팔로잉{' '}
+          </Text>
+          <Text style={{ ...theme.fonts.text.body1.bold, color: theme.colors.graphic.black }}>
+            50K
+          </Text>
         </View>
       </View>
     </View>
--- src/screens/Main/MyPageScreen.tsx
+++ src/screens/Main/MyPageScreen.tsx
@@ -1,25 +1,67 @@
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Pressable } from 'react-native';
 import React from 'react';
 import MypageProfile from '../../components/profile/MypageProfile';
 import { useTheme } from 'react-native-paper';
+import { Feather, Ionicons } from '@expo/vector-icons';
 
 const MyPageScreen = () => {
   const theme = useTheme();
   return (
     <ScrollView>
-      <View style={{ marginLeft: 16, marginBottom: 30 }}>
-        <MypageProfile />
+      <View style={styles.top}>
+        <View style={styles.setting}>
+          <Pressable onPress={() => alert('setting')}>
+            <Ionicons name="settings-outline" size={24} color={`${theme.colors.graphic.black}cc`} />
+          </Pressable>
+          <Pressable onPress={() => alert('more')}>
+            <Feather name="more-vertical" size={24} color={`${theme.colors.graphic.black}cc`} />
+          </Pressable>
+        </View>
+        <View style={{ marginLeft: 16, marginBottom: 24 }}>
+          <MypageProfile />
+        </View>
+        <View style={{ marginHorizontal: 16 }}>
+          <Text
+            style={{ ...theme.fonts.text.body2.regular, color: `${theme.colors.graphic.black}cc` }}
+          >
+            암더 코리안 탑클래스 힙합모범 노블레스 페뷸러스 터뷸렌스 고져스 벗 댕저러스 난 비트를
+            비틀어 제껴버리는 셔브미션 챔피욘
+          </Text>
+        </View>
+        <View style={{ alignItems: 'center' }}>
+          <Pressable style={styles.editBtn} onPress={() => alert('Editing Profile!')}>
+            <Text
+              style={{ ...theme.fonts.text.body1.bold, color: `${theme.colors.graphic.black}cc` }}
+            >
+              프로필 수정
+            </Text>
+          </Pressable>
+        </View>
       </View>
-      <View style={{ marginHorizontal: 16 }}>
-        <Text style={{ ...theme.fonts.text.body2.regular, color: '#121314cc' }}>
-          암더 코리안 탑클래스 힙합모범 노블레스 페뷸러스 터뷸렌스 고져스 벗 댕저러스 난 비트를
-          비틀어 제껴버리는 셔브미션 챔피욘
-        </Text>
-      </View>
+      <View></View>
     </ScrollView>
   );
 };
 
 export default MyPageScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  top: {
+    backgroundColor: '#F1F1E9',
+  },
+  setting: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 18,
+    paddingVertical: 14,
+  },
+  editBtn: {
+    backgroundColor: '#e1e1d0',
+    marginVertical: 32,
+    borderRadius: 12,
+    width: 343,
+    height: 48,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
