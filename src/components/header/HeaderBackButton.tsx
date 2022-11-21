aa874070d87f0e66a5d9c45b32126590982e83b1
watchiswatch <chchih99@gmail.com>
2022-11-21 20:59:01 +0900

    Fix ArrowLeft Icon

--- src/components/header/HeaderBackButton.tsx
+++ src/components/header/HeaderBackButton.tsx
@@ -1,9 +1,10 @@
 import { Pressable } from 'react-native';
 import React from 'react';
-import Icon from 'react-native-vector-icons/AntDesign';
 import { useTheme } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
-
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { SvgXml } from 'react-native-svg';
+import { arrowLeft } from '../../constants/Icons/Navigation/ArrowLeftXml';
 const HeaderBackButton = () => {
   const theme = useTheme();
   const navigation = useNavigation();
@@ -11,7 +12,7 @@ const HeaderBackButton = () => {
   return (
     <>
       <Pressable style={{ marginLeft: 18 }} onPress={() => navigation.goBack()}>
-        <Icon name="arrowleft" size={30} color={theme.colors.graphic.black} />
+        <SvgXml xml={arrowLeft} />
       </Pressable>
     </>
   );
