02b1c4711631a31e0b08fdff73a37ed514324781
Ji-hoon Choi <81629070+akdlsz21@users.noreply.github.com>
2022-12-18 21:37:33 +0900

    [Feat][Feed Screen] 무한 스크롤, 북마크 기능 (#77)
    * feat: infinite scroll on request is done
    
    * style: carousel items are now bigger, and positioned as design;
    
    * style: lottie added, carousel loop falsed
    
    * style: lottie added to login screen
    
    * style: fixed bookmark going out bound of view
    
    * feat: infinite scroll added
    
    * fix: getNextPageParam func fixed, referencing undefined when getting id
    
    * feat: feed screen bookmark is now abled
    
    * style: bookmark loading state is added
    
    * Refactor: Removed Unnecessary if statements,

--- src/screens/Feed/FeedItem.tsx
+++ src/screens/Feed/FeedItem.tsx
@@ -1,20 +1,18 @@
-import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 import React from 'react';
 import { InsightData } from '../../types/Feed/Feedinsights';
 import MiniProfile from '../../components/profile/MiniProfile';
-import { useTheme } from 'react-native-paper';
 import FeedLinkWithBookMark from './FeedLinkCard';
-import { LinkPreview } from '@flyerhq/react-native-link-preview';
 import FeedVerticalDots from './FeedVerticalDots';
 import FeedTextContent from './FeedTextContent';
-import { postFeedBookMark } from '../../utils/api/FeedBookMark';
 
 interface FeedItemProps {
   insight: InsightData;
   onBookMarkClick: (id: number) => void;
+  bookMarkIsLoading: boolean;
 }
 
-const FeedItem = ({ insight, onBookMarkClick }: FeedItemProps) => {
+const FeedItem = ({ insight, onBookMarkClick, bookMarkIsLoading }: FeedItemProps) => {
   const { id, contents, createdAt, link, reaction, writer, bookmark } = insight;
 
   const handleOnBookMarkPress = () => {
@@ -45,7 +43,8 @@ const FeedItem = ({ insight, onBookMarkClick }: FeedItemProps) => {
       <View style={styles.contentCtn}>
         <FeedTextContent contents={contents} insightId={id} />
         <FeedLinkWithBookMark
-          text={'naver.com'}
+          bookMarkIsLoading={bookMarkIsLoading}
+          text={insight.link.url}
           onBookmarkPress={handleOnBookMarkPress}
           isBookMarked={bookmark}
         />
@@ -70,6 +69,8 @@ const styles = StyleSheet.create({
   },
   MiniProfile: {},
   contentCtn: {
+    borderWidth: 1,
+    justifyContent: 'space-between',
     backgroundColor: '#f1f1e9',
     paddingHorizontal: 10,
     paddingVertical: 10,
