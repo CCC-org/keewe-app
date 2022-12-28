9ecf8ab6fabe55733b258d1e5415802775380804
watchiswatch <chchih99@gmail.com>
2022-12-28 19:10:06 +0900

    bottom sheet setting

--- src/screens/Main/mypage/ProfileEditScreen.tsx
+++ src/screens/Main/mypage/ProfileEditScreen.tsx
@@ -1,11 +1,17 @@
 import { StyleSheet, View } from 'react-native';
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useCallback, useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import HeaderRightButton from '../../../components/header/HeaderRightButton';
 import { useTheme } from 'react-native-paper';
 import ProfileList from './ProfileList';
 import { TOTAL_TAG } from '../../../constants/Interests';
 import TwoButtonModal from '../../../components/modal/TwoButtonModal';
 import ProfileImage from './ProfileImage';
+import {
+  BottomSheetBackdrop,
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
+import BottomSheetOption from '../../../components/bottomsheet/BottomSheetOption';
 
 const ProfileEditScreen = ({ navigation, route }) => {
   const theme = useTheme();
@@ -85,39 +91,77 @@ const ProfileEditScreen = ({ navigation, route }) => {
       }),
     );
   }, [route]);
+
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+
+  const snapPoints = useMemo(() => ['30%', '30%'], []);
+
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+
+  const handleDeletePress = () => alert('삭제!');
+  const handleLibraryPress = () => alert('선택!');
+  const handleShotPress = () => alert('찰칵!');
+
+  const renderBackdrop = useCallback(
+    (props) => <BottomSheetBackdrop {...props} disappearsOnIndex={-1} appearsOnIndex={0} />,
+    [],
+  );
   return (
     <>
-      <TwoButtonModal
-        dismissable={false}
-        mainTitle={'수정한 내용을 저장할까요?'}
-        visible={modalVisible}
-        onDismiss={hideModal}
-        leftButtonText={'취소'}
-        rightButtonText={'저장'}
-        leftButtonPress={() => setModalVisible(false)}
-        rightButtonPress={() => setModalVisible(false)}
-      />
-      <ProfileImage onPress={() => alert('hi')} />
-      <View>
-        <ProfileList title="이름" content={nickname} handlePress={handleNickname} />
-        <ProfileList title="대표 타이틀" content={title} handlePress={handleTitle} />
-        <ProfileList
-          title="소개"
-          content={introduction === '' ? '자신을 자유롭게 표현해보세요' : introduction}
-          contentColor={introduction === '' ? `${theme.colors.graphic.black}4d` : undefined}
-          handlePress={handleIntroduction}
-          height={60}
+      <BottomSheetModalProvider>
+        <TwoButtonModal
+          dismissable={false}
+          mainTitle={'수정한 내용을 저장할까요?'}
+          visible={modalVisible}
+          onDismiss={hideModal}
+          leftButtonText={'취소'}
+          rightButtonText={'저장'}
+          leftButtonPress={() => setModalVisible(false)}
+          rightButtonPress={() => setModalVisible(false)}
         />
-        <ProfileList
-          title="관심사"
-          content={selectedCategory.map((cur) => '#' + cur).join(' ')}
-          handlePress={handleInterests}
-        />
-      </View>
+        <ProfileImage onPress={() => handlePresentModalPress()} />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={() => null}
+          backgroundStyle={{ backgroundColor: theme.colors.graphic.white }}
+          backdropComponent={renderBackdrop}
+        >
+          <View style={styles.sheet}>
+            <BottomSheetOption title="현재 사진 삭제" onPress={handleDeletePress} />
+            <BottomSheetOption title="라이브러리에서 선택" onPress={handleLibraryPress} />
+            <BottomSheetOption title="사진 찍기" onPress={handleShotPress} />
+          </View>
+        </BottomSheetModal>
+        <View>
+          <ProfileList title="이름" content={nickname} handlePress={handleNickname} />
+          <ProfileList title="대표 타이틀" content={title} handlePress={handleTitle} />
+          <ProfileList
+            title="소개"
+            content={introduction === '' ? '자신을 자유롭게 표현해보세요' : introduction}
+            contentColor={introduction === '' ? `${theme.colors.graphic.black}4d` : undefined}
+            handlePress={handleIntroduction}
+            height={60}
+          />
+          <ProfileList
+            title="관심사"
+            content={selectedCategory.map((cur) => '#' + cur).join(' ')}
+            handlePress={handleInterests}
+          />
+        </View>
+      </BottomSheetModalProvider>
     </>
   );
 };
 
 export default ProfileEditScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  sheet: {
+    height: 168,
+    justifyContent: 'space-around',
+  },
+});
