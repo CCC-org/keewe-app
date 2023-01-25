0b068c55de10986b0a4cc6b0a6a54d1d2633dc95
akdlsz21 <akdlsz21@gmail.com>
2023-01-25 21:51:57 +0900

    added feedItem list for mypage, infinitequery will be used

--- src/screens/Main/mypage/MyPageFeedList.tsx
+++ src/screens/Main/mypage/MyPageFeedList.tsx
@@ -1,16 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { useInfiniteQuery, useQueryClient } from '@tanstack/react-query';
+import { getAccessToken } from '../../../utils/hooks/asyncStorage/Login';
+import axios from 'axios';
+import { InsightData } from '../../../types/Feed/Feedinsights';
 
 interface Props {
   id: number;
   userId: number;
 }
-
+const URL = 'https://api-keewe.com/api/v1/insight/my-page/';
 const MyPageFeedList = ({ id, userId }: Props) => {
+  const queryClient = useQueryClient();
+
+  const { data: feedList, others } = useInfiniteQuery({
+    queryKey: ['feedList', 'tab', id],
+    queryFn: async (context) => {
+      const token = await getAccessToken();
+      try {
+        const res = await axios.get(URL + String(userId) + `?limit=${100}&cursor=`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        return res.data.data as InsightData[];
+      } catch (err) {
+        alert(err);
+      }
+    },
+  });
+
+  console.log('feedList.data: ', feedList);
+
   return (
     <View>
-      <Text>userId: {userId}</Text>
-      <Text>id: {id}</Text>
+      {Array.isArray(feedList?.pages) &&
+        feedList?.pages.map((group) => {
+          return (
+            <View>
+              <Text>{JSON.stringify(group)}</Text>
+              <Text>-----------------------</Text>
+            </View>
+          );
+        })}
     </View>
   );
 };
