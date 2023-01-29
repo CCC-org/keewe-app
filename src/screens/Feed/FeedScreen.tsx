04ca0732bcebcd72d9bfb0c654d4c7dc5ad0ffd7
akdlsz21 <akdlsz21@gmail.com>
2023-01-29 19:21:28 +0900

    upload button in feedscreen done

--- src/screens/Feed/FeedScreen.tsx
+++ src/screens/Feed/FeedScreen.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import FeedList from './FeedList';
 import { useInfiniteFeed } from '../../utils/hooks/feedInifiniteScroll/useInfiniteFeed';
 import FeedScreenChallenge from '../../components/challenge/FeedScreenChallenge';
-
-const FeedScreen = () => {
+import { SvgXml } from 'react-native-svg';
+import { pencil } from '../../constants/Icons/home/pencil';
+import { Pressable, StyleSheet } from 'react-native';
+const FeedScreen = ({ navigation }) => {
   const {
     feedList,
     feedListIsLoading,
@@ -19,7 +21,9 @@ const FeedScreen = () => {
   //   return <MainLottie />;
   // }
   // <Text style={[theme.fonts.text.display, { marginBottom: 32 }]}>홈</Text>
-
+  const handleNavigateToUpload = () => {
+    navigation.navigate('Upload');
+  };
   return (
     <>
       <FeedList
@@ -31,8 +35,33 @@ const FeedScreen = () => {
         bookMarkIsLoading={bookMarkIsLoading}
         feedListIsLoading={feedListIsLoading}
       />
+      <Pressable style={styles.pencil} onPress={handleNavigateToUpload}>
+        <SvgXml xml={pencil} />
+      </Pressable>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  pencil: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    width: 64,
+    height: 64,
+    borderRadius: 16,
+    backgroundColor: '#b0e817',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
 
 export default FeedScreen;
