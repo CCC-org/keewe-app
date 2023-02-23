68e76bfdac0624b1fa3d7b7fc379035d92e35272
cadi <dbtmdgns4296@naver.com>
2023-02-23 13:20:16 +0900

    hotfix

--- src/screens/Main/Tabs.tsx
+++ src/screens/Main/Tabs.tsx
@@ -1,18 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import BookMarkScreen from './BookMarkScreen';
 import ChallengesScreen from './ChallengesScreen';
 import { RootScreen } from '../../navigation';
 import FeedScreen from '../Feed/FeedScreen';
-import HomeScreen from '../Home/HomeScreen';
-import App from '../../../App';
-import { NavigationContainer } from '@react-navigation/native';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SvgXml } from 'react-native-svg';
 import {
-  homeOn,
-  homeOff,
   bookmarkOn,
   bookmarkOff,
   challengeOn,
@@ -22,16 +16,20 @@ import {
 } from '../../constants/Icons/Navigation/NavigationIconsXml';
 import { useGetUserId } from '../../utils/hooks/useGetUserId';
 import MyPageScreen from './mypage/MyPageScreen';
-const queryClient = new QueryClient();
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
   const userId = useGetUserId();
   return (
-    <QueryClientProvider client={queryClient}>
-      <Tab.Navigator
-        screenOptions={{
-          tabBarShowLabel: false,
+    <Tab.Navigator
+      screenOptions={{
+        tabBarShowLabel: false,
+      }}
+    >
+      <Tab.Screen
+        options={{
+          tabBarIcon: ({ focused }) =>
+            focused ? <SvgXml xml={challengeOn} /> : <SvgXml xml={challengeOff} />,
         }}
         name="Feed"
         component={FeedScreen}
