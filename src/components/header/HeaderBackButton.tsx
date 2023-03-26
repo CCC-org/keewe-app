0a270fbea431a14763af86a67c83a32192e2510c
watchiswatch <chchih99@gmail.com>
2023-03-26 16:41:09 +0900

    import some Icons by SvgXml & deleted some unnessesary imported things.

--- src/components/header/HeaderBackButton.tsx
+++ src/components/header/HeaderBackButton.tsx
@@ -1,12 +1,9 @@
 import { Pressable } from 'react-native';
 import React from 'react';
-import { useTheme } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { SvgXml } from 'react-native-svg';
 import { arrowLeft } from '../../constants/Icons/Navigation/ArrowLeftXml';
 const HeaderBackButton = () => {
-  const theme = useTheme();
   const navigation = useNavigation();
 
   return (
