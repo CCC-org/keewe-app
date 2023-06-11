7a020734685da015808401611e9a8ac504e8c26b
watchiswatch <chchih99@gmail.com>
2023-06-11 23:26:20 +0900

    bookmark 첫 cursor date 한국 시간으로 변경

--- src/utils/api/FeedAPI.ts
+++ src/utils/api/FeedAPI.ts
@@ -19,9 +19,18 @@ export const FeedAPI = {
     return response.data.data;
   },
   getBookMarkFeed: async (cursor: string, limit: number) => {
-    const currentDate = new Date().toISOString().slice(0, 23);
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+    const day = String(currentDate.getDate()).padStart(2, '0');
+    const hours = String(currentDate.getHours()).padStart(2, '0');
+    const minutes = String(currentDate.getMinutes()).padStart(2, '0');
+    const seconds = String(currentDate.getSeconds()).padStart(2, '0');
+    const milliseconds = String(currentDate.getMilliseconds()).padStart(3, '0');
+
+    const formattedDate = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}`;
     const URL = `https://api-keewe.com/api/v1/insight/bookmark?cursor=${
-      !cursor ? currentDate : cursor.slice(0, 23)
+      !cursor ? formattedDate : cursor.slice(0, 23)
     }&limit=${String(limit)}`;
     try {
       const response = await httpClient.get<FeedInsight>(URL);
