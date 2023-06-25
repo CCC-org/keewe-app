26df47e11cf17537540f4e13f5027057f38c4219
watchiswatch <chchih99@gmail.com>
2023-06-25 20:21:53 +0900

    unused import 제거

--- src/components/bottomsheet/SheetMyPostOptions.tsx
+++ src/components/bottomsheet/SheetMyPostOptions.tsx
@@ -1,4 +1,4 @@
-import { Pressable, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { Pressable, ScrollView, StyleSheet, Text } from 'react-native';
 import React, { useState } from 'react';
 import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
 import { useTheme } from 'react-native-paper';
@@ -6,7 +6,7 @@ import TwoButtonModal from '../modal/TwoButtonModal';
 import { Toast } from 'react-native-toast-message/lib/src/Toast';
 import { useNavigation } from '@react-navigation/native';
 import httpClient from '../../utils/api/BaseHttpClient';
-import { QueryClient, useQueryClient } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 import { FeedQueryKeys } from '../../utils/api/FeedAPI';
 
 interface BSMyPostOptionsProps {
