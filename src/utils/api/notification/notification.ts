7999bf8c7d076f6a78984a76ab86dd48ff7a0f82
ysh4296 <dbtmdgns4296@naver.com>
2023-06-26 12:07:46 +0900

    [QA][알람] 알람 아이콘 추가 (#172)
    - 신규 알람 있을 시 아이콘 추가

--- src/utils/api/notification/notification.ts
+++ src/utils/api/notification/notification.ts
@@ -1,25 +1,19 @@
-import { Notification } from '../../../types/notification/notification';
-import { getAccessToken } from '../../hooks/asyncStorage/Login';
 import httpClient from '../BaseHttpClient';
 
 export const notificationKeys = {
   getNotificationList: () => ['notifications'],
+  checkNotification: () => ['notifications', 'check'],
 };
 
 export const notificationApi = {
   getNotificationList: async (
     cursor: number | string | undefined | null,
   ): Promise<Notification | null> => {
-    const token = await getAccessToken();
     const URL = `https://api-keewe.com/api/v1/notification?limit=10${
       cursor ? '&cursor=' + cursor : ''
     }`;
     try {
-      const response = await httpClient.get(URL, {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
+      const response = await httpClient.get(URL);
       if (response.data.code !== 200) {
         throw new Error('get notificationList error');
       }
@@ -32,17 +26,25 @@ export const notificationApi = {
       return null;
     }
   },
+  checkNotification: async () => {
+    try {
+      const response = await httpClient.get<CheckNotification>(
+        'https://api-keewe.com/api/v1/notification/unread-existence',
+      );
+      console.log(response.data);
+      const data = response.data.data;
+      return data;
+    } catch (e) {
+      if (e instanceof Error) {
+        console.error(e.message);
+      }
+      return null;
+    }
+  },
   patchMarkAsRead: async (id: number | string): Promise<boolean> => {
-    const token = await getAccessToken();
     try {
       const response = await httpClient.patch(
         `https://api-keewe.com/api/v1/notification/${id}/read`,
-        {},
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        },
       );
       if (response.data.code !== 200) {
         throw new Error('patch mark as read error');
