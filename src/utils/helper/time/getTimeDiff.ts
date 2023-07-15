e6cb430d2a9d66d00afca4bfa7e2024c5492a077
winteryou <winter.you@daangnpay.com>
2023-07-15 14:54:57 +0900

    알림 생성 시간이 1일 이내면 시간으로 표현하도록 수정

--- src/utils/helper/time/getTimeDiff.ts
+++ src/utils/helper/time/getTimeDiff.ts
@@ -5,6 +5,9 @@ export function getTimeDiff(dateStr: FormattedDate): string {
   const now = new Date();
   const diffTime = Math.abs(now.getTime() - date.getTime());
 
+  // Get diff in hours
+  const diffHours = Math.floor(diffTime / (1000 * 60 * 60));
+
   // Get diff in days
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
@@ -14,7 +17,9 @@ export function getTimeDiff(dateStr: FormattedDate): string {
   // Get diff in months
   const diffMonths = Math.floor(diffDays / 30);
 
-  if (diffDays <= 6) {
+  if (diffHours < 24) {
+    return `${diffHours}시간 전`;
+  } else if (diffDays <= 6) {
     return `${diffDays}일 전`;
   } else if (diffWeeks <= 4) {
     return `${diffWeeks}주 전`;
