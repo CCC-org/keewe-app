c9aa7f620e89ed39828c648ce70fe9d3ef332130
ysh4296 <dbtmdgns4296@naver.com>
2023-07-25 16:33:23 +0900

    [Feat][알람] expo 권한설정 추가 (#220)
    * expo token 로직 추가
    
    * 알람 권한설정 추가

--- App.tsx
+++ App.tsx
@@ -16,7 +16,8 @@ import * as Notifications from 'expo-notifications';
 
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { createStackNavigator } from '@react-navigation/stack';
-import { Pressable, View } from 'react-native';
+import { Platform, Pressable, View } from 'react-native';
+import * as Device from 'expo-device';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider as PaperProvider } from 'react-native-paper';
 import OnboardingIntroHeaderButton from './src/components/buttons/OnboardingIntroHeaderButton';
@@ -96,10 +97,39 @@ const getExpoToken = async () => {
   setExpoToken(token);
 };
 
+const registerForPushNotificationsAsync = async () => {
+  if (Platform.OS === 'android') {
+    await Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } = await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== 'granted') {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    if (finalStatus !== 'granted') {
+      alert('Failed to get push token for push notification!');
+      return;
+    }
+    getExpoToken();
+  } else {
+    alert('Must use physical device for Push Notifications');
+  }
+
+  return;
+};
+
 export default function App() {
   const isLoadingComplete = useCachedResources();
   getDeviceToken();
-  getExpoToken();
+  registerForPushNotificationsAsync();
   if (!isLoadingComplete) {
     return null;
   } else {
