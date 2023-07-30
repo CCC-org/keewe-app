4683ef16bfc9d69c3f85add8b40f2007e49fcaa7
ysh4296 <dbtmdgns4296@naver.com>
2023-07-30 23:23:24 +0900

    [Fix][앱링크] 링크 이동로직 변경 (#230)
    - 이동로직 변경

--- src/screens/link/LinkScreen.tsx
+++ src/screens/link/LinkScreen.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import MainLottie from '../../components/lotties/MainLottie';
 import { ChallengeAPI, ChallengeQueryKeys } from '../../utils/api/ChallengeAPI';
 import { useGetUserId } from '../../utils/hooks/useGetUserId';
@@ -12,66 +12,66 @@ const LinkScreen = ({ navigation, route }) => {
   useQuery(ChallengeQueryKeys.getParticipationCheck(), () => ChallengeAPI.getParticipationCheck(), {
     onSuccess: () => setParticipated(true),
   });
-  const { data: challengeParticipation, isLoading: isChallengeParticipationLoading } = useQuery(
-    ChallengeQueryKeys.getChallengeParticipation(),
-    ChallengeAPI.getChallengeParticipation,
-    { enabled: participated },
-  );
 
-  useEffect(() => {
-    if (type === 'insight') {
+  if (type === 'insight') {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Tabs' }],
+    });
+    navigation.push('DetailedPost', {
+      screen: 'DetailedPost',
+      insightId: id,
+    });
+  }
+  if (type === 'profile') {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Tabs' }],
+    });
+    if (userId === id) {
+      navigation.push('MyPage', {
+        screen: 'MyPage',
+        userId: id,
+      });
+    } else {
       navigation.reset({
         index: 0,
         routes: [{ name: 'Tabs' }],
       });
-      navigation.push('DetailedPost', {
-        screen: 'DetailedPost',
-        insightId: id,
+      navigation.push('Profile', {
+        screen: 'Profile',
+        userId: id,
       });
     }
-    if (type === 'profile') {
-      navigation.reset({
-        index: 0,
-        routes: [{ name: 'Tabs' }],
-      });
-      if (userId === id) {
-        navigation.push('MyPage', {
-          screen: 'MyPage',
-          userId: id,
-        });
-      } else {
-        navigation.reset({
-          index: 0,
-          routes: [{ name: 'Tabs' }],
-        });
-        navigation.push('Profile', {
-          screen: 'Profile',
-          userId: id,
-        });
-      }
-    }
-    if (type === 'challenge') {
-      if (!isChallengeParticipationLoading) {
-        navigation.reset({
-          index: 0,
-          routes: [{ name: 'Tabs' }],
-        });
-        if (participated && challengeParticipation?.challengeId === id) {
-          navigation.push('ChallengeDetail', {
-            screen: 'ChallengeDetail',
-            challengeId: challengeParticipation?.challengeId,
-            challengeName: challengeParticipation?.name,
-            interest: challengeParticipation?.interest,
-          });
-        } else {
-          navigation.push('ChallengeParticipation', {
-            screen: 'ChallengeParticipation',
-            challengeId: id,
-          });
-        }
-      }
-    }
-  }, [isChallengeParticipationLoading]);
+  }
+  if (type === 'challenge') {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: 'Tabs' }],
+    });
+    const { data: challengeParticipation, isLoading: isChallengeParticipationLoading } = useQuery(
+      ChallengeQueryKeys.getChallengeParticipation(),
+      ChallengeAPI.getChallengeParticipation,
+      {
+        onSuccess: (response) => {
+          if (response.challengeId === id) {
+            navigation.push('ChallengeDetail', {
+              screen: 'ChallengeDetail',
+              challengeId: challengeParticipation?.challengeId,
+              challengeName: challengeParticipation?.name,
+              interest: challengeParticipation?.interest,
+            });
+          } else {
+            navigation.push('ChallengeParticipation', {
+              screen: 'ChallengeParticipation',
+              challengeId: id,
+            });
+          }
+        },
+        enabled: participated,
+      },
+    );
+  }
 
   return (
     <>
