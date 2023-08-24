5d316518e4c8ab2f4bdfad269fcd372282984f65
ysh4296 <dbtmdgns4296@naver.com>
2023-08-24 20:29:41 +0900

    [Refactor][리액션] react-native-flying-objects 적용 (#248)
    * react native flying objects 적용
    
    * 서체 임시 적용
    
    * 적용

--- src/components/emoticons/ReactIconButton.tsx
+++ src/components/emoticons/ReactIconButton.tsx
@@ -1,10 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { Animated } from 'react-native';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
+import { Animated, Easing } from 'react-native';
 import { SvgXml } from 'react-native-svg';
 import { useMutation } from '@tanstack/react-query';
-import { Pressable, Text, View, StyleSheet } from 'react-native';
-import FlyingEmoticons from './FlyingEmoticons';
+import { Pressable, Text, StyleSheet } from 'react-native';
 import { InsightAPI } from '../../utils/api/InsightAPI';
+import { FlyingView, ObjectConfig } from 'react-native-flying-objects';
 import theme from '../../theme/light';
 
 interface ReactIconButtonProps {
@@ -17,7 +17,7 @@ interface ReactIconButtonProps {
 
 const ReactIconButton = ({ xml, color, taps, name, insightId }: ReactIconButtonProps) => {
   const opacityValue = useRef(new Animated.Value(0)).current;
-  const [animate, setAnimate] = useState<string[]>([]);
+  const [object, setObject] = useState<ObjectConfig[]>([]);
   const [text, setText] = useState<number>();
   const { mutate: insightReact } = useMutation(InsightAPI.react, {
     onSuccess: (response) => {
@@ -31,7 +31,7 @@ const ReactIconButton = ({ xml, color, taps, name, insightId }: ReactIconButtonP
 
   const handleClick = () => {
     insightReact({ insightId, reactionType: name, value: 1 });
-    setAnimate((prev) => [...prev, `${xml}${animate.length}`]);
+    setObject((prev) => [...prev, objectConfig]);
     Animated.timing(opacityValue, {
       toValue: 1,
       duration: 0,
@@ -43,6 +43,37 @@ const ReactIconButton = ({ xml, color, taps, name, insightId }: ReactIconButtonP
       useNativeDriver: false,
     }).start();
   };
+
+  const objectConfig = useMemo(
+    () => ({
+      right: {
+        fromValue: 15,
+        toValue: Math.floor(Math.random() * 30),
+        duration: 1200,
+        easing: Easing.bezier(
+          Math.floor(Math.random() * 10) / 10,
+          Math.floor(Math.random() * 10) / 10 + 0.7,
+          Math.floor(Math.random() * 10) / 10,
+          Math.floor(Math.random() * 10) / 10 + 0.7,
+        ),
+      },
+      top: {
+        fromValue: 100,
+        toValue: 35,
+        duration: 1200,
+        easing: Easing.bezier(0.5, 1.0, 0.5, 1.0),
+      },
+      show: {
+        fromValue: 1,
+        toValue: 1,
+      },
+      hide: {
+        fromValue: 1,
+        toValue: 0,
+      },
+    }),
+    [object],
+  );
 
   return (
     <>
@@ -66,27 +97,22 @@ const ReactIconButton = ({ xml, color, taps, name, insightId }: ReactIconButtonP
                   inputRange: [0, 1],
                   outputRange: [theme.colors.graphic.black, theme.colors.graphic.white],
                 }),
-                ...theme.fonts.text.podkova.bold,
                 marginLeft: 4,
+                fontFamily: 'podkovaBold',
+                fontSize: 14,
+                lineHeight: 16,
               }}
             >
               <Text>{text}</Text>
             </Animated.Text>
           )}
         </Animated.View>
-        <View
-          pointerEvents="none"
-          style={{
-            width: 50,
-            height: 100,
-            position: 'absolute',
-            bottom: 20,
-          }}
+        <FlyingView
+          object={object}
+          containerProps={{ style: { position: 'absolute', bottom: 20 } }}
         >
-          {animate.map((animateValue) => (
-            <FlyingEmoticons key={animateValue} xml={xml} width={16} height={16} />
-          ))}
-        </View>
+          <SvgXml xml={xml} height={20} width={20} />
+        </FlyingView>
       </Pressable>
     </>
   );
